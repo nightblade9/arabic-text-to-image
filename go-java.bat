c25cfbed6146d68de8659e2bdd14f483bb21d063
ahaleem <alia.haleem@spaceishare.ml>
2018-06-13 16:40:09 -0400

    Fix environment dependency on UTF-8 encoding to JAVAC

--- go-java.bat
+++ go-java.bat
@@ -1,5 +1,5 @@
 rd /s /q output
 mkdir output
 
-javac -cp "gson-2.8.2.jar;"  TextToGraphics.java Word.java
+"C:\Program Files\Java\jdk1.8.0_162\bin\javac.exe" -encoding UTF-8 -cp "gson-2.8.2.jar;"  TextToGraphics.java Word.java
 java -cp ".;gson-2.8.2.jar" TextToGraphics
